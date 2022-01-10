b36f66d223ba204e84fbb7e50b85bf44fb989b31
Oscar <o@ovl.design>
2022-01-10 12:17:28 +0100

    feat(stack): remove global styles

--- _src/_includes/components/core/TheStack.vue
+++ _src/_includes/components/core/TheStack.vue
@@ -15,44 +15,15 @@ export default {
   props: {
     tagName: {
       type: String,
-      default: 'div'
+      default: 'div',
     },
     flows: {
       type: String,
       default: 'vertical',
-      validator: flow => flow === 'vertical' || flow === 'horizontal'
-    }
-  }
+      validator: (flow) => flow === 'vertical' || flow === 'horizontal',
+    },
+  },
 }
 </script>
 
-<style lang="scss">
-.l-stack {
-  --stack-space: 1.5rem;
-  --flex-direction: row;
-
-  display: flex;
-  flex-direction: var(--flex-direction);
-
-  &--horizontal {
-    align-items: center;
-
-    & > * + * {
-      margin-left: var(--stack-space);
-    }
-  }
-
-  &--vertical {
-    --flex-direction: column;
-
-    & > * + * {
-      margin-top: var(--stack-space);
-      margin-bottom: 0 !important;
-    }
-  }
-
-  &--narrow {
-    --stack-space: 0.5rem;
-  }
-}
-</style>
+<style></style>
