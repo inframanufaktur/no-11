c599fdd9440df0b2080841c06bcd5d9dc2dbda0a
Oscar <o@ovl.design>
2022-01-15 16:03:44 +0100

    feat(css): automatically create css entry points

--- _src/_data/css.js
+++ _src/_data/css.js
@@ -1,18 +1,31 @@
 const path = require('path')
-
+const fs = require('fs')
+const { camelCase } = require('lodash')
 const { compileCss } = require(path.join(
   process.cwd(),
   '_helper/compileCss.js',
 ))
+const STATIC_FOLDERS = require(path.join(process.cwd(), '_helper/paths.js'))
 
-const partials = {
-  // code: 'code.css',
-  customProperties: 'custom-properties.css',
-  // footer: 'footer.css',
-  // home: 'home.css',
-  main: 'main.css',
-  // text: 'text.css',
-  // textDetail: 'text-detail.css',
-}
+const cssPath = path.join(process.cwd(), STATIC_FOLDERS.css)
+
+const partials = {}
+
+fs.readdir(cssPath, function (err, files) {
+  if (err) {
+    throw new Error('Unable to scan directory: ' + err)
+  }
+
+  files.forEach(function (fileName) {
+    if (fileName.endsWith('.css')) {
+      const property = camelCase(fileName.replace('.css', ''))
+
+      Object.defineProperty(partials, property, {
+        value: fileName,
+        enumerable: true,
+      })
+    }
+  })
+})
 
 module.exports = compileCss(partials)
