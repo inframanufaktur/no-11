992cc0833e6844eb2cfbfb0e2f70738260bf92b9
Oscar <o@ovl.design>
2022-01-18 17:36:50 +0100

    feat(nav): allow to set classes and attributes on data level

--- _shortcodes/nav-link.js
+++ _shortcodes/nav-link.js
@@ -4,6 +4,7 @@ module.exports = {
   name: 'navLink',
   shortcodeFunction: function (menuItem, extraClasses = '') {
     const { url } = this.page
+    const { attributes = {}, classes = '' } = menuItem
 
     if (typeof extraClasses !== 'string') {
       throw new Error(
@@ -11,19 +12,29 @@ module.exports = {
       )
     }
 
-    let ariaCurrent = url === menuItem.url ? 'aria-current="page"' : ''
-    let linkClasses = extraClasses
-      ? `${BASE_LINK_CLASSES} ${extraClasses.trim()}`
-      : BASE_LINK_CLASSES
+    let activeClass = ''
 
+    // highlight menu item if sub page is visited
+    // e.g. `/blog/blog-post` should highlight `/blog/`
+    // check for exact here, as `/` would always be highlighted
     if (url.includes(menuItem.url) && !menuItem.exact) {
-      linkClasses += ' -is-active'
+      activeClass = ' -is-active'
     }
 
+    // highlight exact match, mostly for homepage or something pretty specific
     if (url === menuItem.url && menuItem.exact) {
-      linkClasses += ' -is-active'
+      activeClass = ' -is-active'
     }
 
-    return `<a href="${menuItem.url}" class="${linkClasses}" ${ariaCurrent}>${menuItem.title}</a>`
+    let ariaCurrent = url === menuItem.url ? 'aria-current="page"' : ''
+    let linkClasses = [BASE_LINK_CLASSES, classes, extraClasses, activeClass]
+      .join(' ')
+      .trim()
+
+    let extraAttributes = Object.keys(attributes)
+      .map((key) => `${key}="${attributes[key]}"`)
+      .join(' ')
+
+    return `<a href="${menuItem.url}" class="${linkClasses}" ${extraAttributes} ${ariaCurrent}>${menuItem.title}</a>`
   },
 }
