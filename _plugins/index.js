0373d6eccbca3fe2966341904a47d87f8dcc7168
Oscar <o@ovl.design>
2022-03-24 09:59:05 +0100

    feat(sitemap): render sitemap && add robots file

--- _plugins/index.js
+++ _plugins/index.js
@@ -1,6 +1,10 @@
 const render = require('./render')
 const rss = require('./rss')
+const sitemap = require('./sitemap')
 const syntaxHighlight = require('./syntax-highlight')
 const vue = require('./vue')
 
-module.exports = { always: [vue, render, rss, syntaxHighlight], prod: [] }
+module.exports = {
+  always: [vue, render, rss, syntaxHighlight, sitemap],
+  prod: [],
+}
--- _src/css.11ty.js
+++ _src/css.11ty.js
@@ -2,6 +2,7 @@ module.exports = class {
   data() {
     return {
       pagination: { data: 'css', size: 1, alias: 'cssEntry' },
+      eleventyExcludeFromCollections: true,
       layout: null,
       permalink: (data) => {
         return data.css[data.cssEntry].permalink
