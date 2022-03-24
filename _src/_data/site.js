f797314f1d200a863fff37da5d07b86efe4abdec
Oscar <o@ovl.design>
2022-03-24 18:48:36 +0100

    feat(global data): rename meta to site

--- _src/_data/site.js
+++ _src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
   title: 'Inframanufaktur No. 11',
   description: 'An 11ty starter with an orchestra of bells and whistles.',
-  hostname: process.env.BASE_URL,
+  baseURL: process.env.BASE_URL,
 }
