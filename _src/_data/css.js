b256131d434ef28f7043f429394d852070127bc6
Oscar <o@ovl.design>
2022-03-29 20:46:45 +0200

    feat(css): use getFilesOfType for module recognition

--- _src/_data/css.js
+++ _src/_data/css.js
@@ -1,27 +1,25 @@
 const path = require('path')
-const fs = require('fs')
 const { camelCase } = require('lodash')
 const { compileCss } = require(path.join(
   process.cwd(),
   '_helper/compile-css.js',
 ))
+const getFilesOfType = require(path.join(process.cwd(), '_helper/get-files.js'))
 const STATIC_FOLDERS = require(path.join(process.cwd(), '_helper/paths.js'))
 
 const cssPath = path.join(process.cwd(), STATIC_FOLDERS.css)
 
 const partials = {}
 
-fs.readdirSync(cssPath)
-  .filter((fileName) => fileName.endsWith('.css'))
-  .forEach(function (fileName) {
-    const property = camelCase(fileName.replace('.css', ''))
+getFilesOfType(cssPath, '.css').forEach(function (fileName) {
+  const property = camelCase(fileName.replace('.css', ''))
 
-    Object.defineProperty(partials, property, {
-      value: fileName,
-      enumerable: true,
-      writable: true,
-      configurable: true,
-    })
+  Object.defineProperty(partials, property, {
+    value: fileName,
+    enumerable: true,
+    writable: true,
+    configurable: true,
   })
+})
 
 module.exports = compileCss(partials)
