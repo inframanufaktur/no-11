9dc276c4c205e0c01bcd0c7646d3609f8ca8dbc2
Oscar <o@ovl.design>
2024-05-29 21:37:34 +0200

    chore(postcss): rm custom properties

--- _helper/postcss/index.js
+++ _helper/postcss/index.js
@@ -1,15 +1,7 @@
 const path = require('path')
 const postcss = require('postcss')
-const postcssCustomProperties = require('postcss-custom-properties')
 const postcssJitProps = require('postcss-jit-props')
 const OpenProps = require('open-props')
-
-const STATIC_FOLDERS = require('../paths')
-const propertiesPath = path.join(
-  process.cwd(),
-  STATIC_FOLDERS.css,
-  'custom-properties.css',
-)
 
 let PLUGINS = [
   require('postcss-mixins')({
@@ -17,8 +9,6 @@ let PLUGINS = [
   }),
   require('postcss-import'),
   require('postcss-nested'),
-  require('postcss-custom-properties')({ preserve: true }),
-  postcssCustomProperties({ importFrom: propertiesPath }),
   postcssJitProps(OpenProps),
   require('autoprefixer'),
 ]
